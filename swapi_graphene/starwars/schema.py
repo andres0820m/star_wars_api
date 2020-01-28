f425017d4e57aa55ecff2a80adec3434fef1b487
andresmejiajimenez <a.mejia@transportsystems.co>
2020-01-28 07:22:08 -0500

    listo los filtros necesarios para hacer las busquedas requeridas

--- swapi_graphene/starwars/schema.py
+++ swapi_graphene/starwars/schema.py
@@ -3,31 +3,43 @@ from graphene import relay, ObjectType
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 import graphene
-from .models import People, Planet
+from .models import People, Planet, Film
 
 
 class PeopleNode(DjangoObjectType):
     class Meta:
         model = People
-        filter_fields = ['name', 'mass', 'homeworld']
-        interfaces = (relay.Node,)
 
 
 class PlanetNode(DjangoObjectType):
     class Meta:
         model = Planet
-        filter_fields = ['name']
-        interfaces = (relay.Node,)
+
+
+class FilmNode(DjangoObjectType):
+    class Meta:
+        model = Film
 
 
 class Query(ObjectType):
-    people = relay.Node.Field(PeopleNode)
+    people = graphene.Field(PeopleNode, name=graphene.String())
     all_people = graphene.List(PeopleNode)
     all_planets = graphene.List(PlanetNode)
-    people_filter = DjangoFilterConnectionField(PeopleNode)
+    film = graphene.List(FilmNode, characters=graphene.String())
+
+    # people_filter = DjangoFilterConnectionField(PeopleNode)
 
     def resolve_all_planets(self, info, **kwargs):
         return Planet.objects.select_related('name').all()
 
-    def resolve_all_people(self, info, **kwargs):
+    def resolve_all_people(self, _, __):
         return People.objects.all()
+
+    def resolve_people(self, _, **kwargs):
+        name = kwargs.get('name')
+        if name is not None:
+            return People.objects.get(name=name)
+
+    def resolve_film(self, _, **kwargs):
+        character = People.objects.get(name=kwargs.get('characters'))
+        return Film.objects.filter(characters=character)
