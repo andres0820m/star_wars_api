f72c6a6ce9b70ec8bdba5a7e57824ed60fcb360d
andresmejiajimenez <a.mejia@transportsystems.co>
2020-01-30 07:53:06 -0500

    test

--- swapi_graphene/entrypoint.sh
+++ swapi_graphene/entrypoint.sh
@@ -2,4 +2,5 @@
 python swapi_graphene/manage.py makemigrations
 python swapi_graphene/manage.py migrate --noinput
 python swapi_graphene/manage.py loaddata /usr/local/src/star_wars_api/swapi_graphene/*/fixtures/*.json
+python swapi_graphene/manage.py test
 python swapi_graphene/manage.py runserver 0.0.0.0:8000
